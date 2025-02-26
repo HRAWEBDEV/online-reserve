93deee89cafb31095107ae10c5ac2a000bacf2a6
hamidwise1 <hrawebdev@gmail.com>
2025-02-26 14:36:04 +0330

    add room Daily Price

--- app/[locale]/(website)/(main)/(home)/search/hotel/services/HotelApiActions.ts
+++ app/[locale]/(website)/(main)/(home)/search/hotel/services/HotelApiActions.ts
@@ -3,8 +3,8 @@ import { axios } from '@/app/services/axios/axiosBaseConfig';
 const getRoomInventoryKey = 'get-room-inventory';
 const getRoomInventoriesApi = '/CRS/OnlineReservation/GetRoomInventories';
 
-const getRoomsDailyPriceKey = '';
-const getRoomsDailyPriceKey = '';
+const getRoomDailyPriceKey = 'get-room-daily-price';
+const getRoomDailyPriceApi = '/CRS/OnlineReservation/GetMonthyInventory';
 
 type RoomAccomodationType = {
  roomOnlineShowRate: number;
@@ -37,6 +37,15 @@ type RoomInventory = {
  accommodationTypePrices: RoomAccomodationType[];
 };
 
+type RoomDailyPrice = {
+ date: string;
+ roomOnlineShowRate: number;
+ netRoomRate: number;
+ stopSell: boolean;
+ cta: boolean;
+ ctd: boolean;
+};
+
 function getRoomInventory({
  signal,
  ...queries
@@ -53,7 +62,44 @@ function getRoomInventory({
  Object.entries(queries).forEach(([key, val]) => {
   searchParams.set(key, String(val));
  });
- return axios.get<RoomInventory[]>(getRoomInventoriesApi, { signal });
+ return axios.get<RoomInventory[]>(
+  `${getRoomInventoriesApi}${searchParams.toString()}`,
+  { signal }
+ );
 }
 
-export { type RoomInventory, getRoomInventoryKey, getRoomInventory };
+function getRoomPriceDaily({
+ signal,
+ ...queries
+}: {
+ signal: AbortSignal;
+ channelID: number;
+ hotelID: number;
+ providerID: number;
+ startDate: string;
+ endDate: string;
+ roomTypeID: number;
+ ratePlanID: number;
+ arzID: number;
+ beds: number;
+}) {
+ const searchParams = new URLSearchParams();
+ Object.entries(queries).forEach(([key, val]) => {
+  searchParams.set(key, String(val));
+ });
+ return axios.get<RoomDailyPrice>(
+  `${getRoomDailyPriceApi}${searchParams.toString()}`,
+  {
+   signal,
+  }
+ );
+}
+
+export {
+ type RoomInventory,
+ type RoomDailyPrice,
+ getRoomInventoryKey,
+ getRoomInventory,
+ getRoomDailyPriceKey,
+ getRoomPriceDaily,
+};
