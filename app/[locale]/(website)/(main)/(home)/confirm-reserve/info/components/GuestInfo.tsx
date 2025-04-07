6f5dd8d36deb83c2f960e4af27cabec9347a93b7
hamidwise1 <hrawebdev@gmail.com>
2025-04-07 08:38:08 +0330

    add reserve info

--- app/[locale]/(website)/(main)/(home)/confirm-reserve/info/components/GuestInfo.tsx
+++ app/[locale]/(website)/(main)/(home)/confirm-reserve/info/components/GuestInfo.tsx
@@ -18,31 +18,34 @@ export default function GuestInfo() {
     </Button>
    </div>
    <div className='grid grid-cols-2 gap-4 mb-6'>
-    <TextField size='medium' label='نام و نام‌خانوادگی' />
-    <TextField size='medium' label='کدملی' />
+    <TextField size='medium' label='نام و نام‌خانوادگی' required />
+    <TextField size='medium' label='کدملی' required />
     <TextField size='medium' label='پست الکترونیکی' />
-    <TextField size='medium' label='شماره همراه' />
+    <TextField size='medium' label='شماره همراه' required />
    </div>
    <h3 className='font-medium text-base mb-6'>مشخصات سرپرست اتاق‌ها</h3>
-   <div className='mb-4'>
-    <div className='flex gap-2 items-center mb-4'>
-     <p>
-      <span className='text-neutral-600'>اتاق ۱: </span>
-      <span className='font-medium text-[0.9rem]'>سوئیت دبل</span>
-     </p>
-     <div className='bg-neutral-300 h-[1px] flex-grow'></div>
-    </div>
-    <div className='grid grid-cols-2 gap-4 mb-6'>
-     <div className='col-span-full'>
-      <FormControlLabel
-       label='نام رزرو کننده با سرپرست اتاق یکی می‌باشد.'
-       control={<Checkbox />}
-      />
+   {[1, 2].map((item) => (
+    <div key={item} className='mb-4'>
+     <div className='flex gap-2 items-center mb-4'>
+      <p>
+       <span className='text-neutral-600'>اتاق {item}: </span>
+       <span className='font-medium text-[0.9rem]'>سوئیت دبل</span>
+      </p>
+      <div className='bg-neutral-300 h-[1px] flex-grow'></div>
      </div>
-     <TextField size='medium' label='نام و نام‌خانوادگی' />
-     <TextField size='medium' label='کدملی' />
+     <div className='grid grid-cols-2 gap-4 mb-6'>
+      <div className='col-span-full'>
+       <FormControlLabel
+        label='نام رزرو کننده با سرپرست اتاق یکی می‌باشد.'
+        control={<Checkbox />}
+       />
+      </div>
+      <TextField size='medium' label='نام و نام‌خانوادگی' />
+      <TextField size='medium' label='کدملی' />
+     </div>
     </div>
-   </div>
+   ))}
+
    <div className='flex justify-end'>
     <Button className='w-[10rem]' size='large' variant='contained'>
      تایید
