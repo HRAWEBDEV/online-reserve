9757ee9412d96fdff5c63ee294d37813ed566295
hamidwise1 <hrawebdev@gmail.com>
2025-04-22 10:01:25 +0330

    add get selected rooom api action

--- app/[locale]/(website)/(main)/(home)/confirm-reserve/info/services/addRoomsApiActions.ts
+++ app/[locale]/(website)/(main)/(home)/confirm-reserve/info/services/addRoomsApiActions.ts
@@ -2,6 +2,8 @@ import { axios } from '@/app/services/axios/axiosBaseConfig';
 import { type RoomInventory } from '../../../search/hotel/services/HotelApiActions';
 
 const getRoomsInfoKey = 'get-rooms-info';
+const getSelectedRoomKey = 'get-selected-room';
+const getSelectedRoomApi = '/CRS/OnlineReservation/AccommodationTypeInfo';
 const getSelectedRoomsKey = 'get-selected-rooms';
 const getSelectedRoomsApi = '/CRS/OnlineReservation/GetAccommodationTypes';
 
@@ -37,6 +39,32 @@ const getSelectedRooms = ({
   }
  );
 };
+const getSelectedRoom = ({
+ signal,
+ ...queries
+}: {
+ signal: AbortSignal;
+ channelID: number;
+ hotelID: number;
+ providerID: number;
+ arzID: number;
+ startDate: string;
+ endDate: string;
+ ratePlanID: number;
+ bedCount: number;
+ roomTypeID: number;
+}) => {
+ const searchParams = new URLSearchParams();
+ Object.entries(queries).forEach(([key, val]) => {
+  searchParams.set(key, String(val));
+ });
+ return axios.get<RoomInventory>(
+  `${getSelectedRoomApi}?${searchParams.toString()}`,
+  {
+   signal,
+  }
+ );
+};
 
 export {
  type RoomInfo,
@@ -44,4 +72,6 @@ export {
  getRoomsInfoKey,
  getSelectedRoomsKey,
  getSelectedRooms,
+ getSelectedRoomKey,
+ getSelectedRoom,
 };
