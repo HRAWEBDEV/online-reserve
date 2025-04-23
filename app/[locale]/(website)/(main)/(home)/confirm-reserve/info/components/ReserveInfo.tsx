21e5829cb50ae1b7680774f81230b8c14d518e59
hamidwise1 <hrawebdev@gmail.com>
2025-04-23 12:18:52 +0330

    update discount percent info

--- app/[locale]/(website)/(main)/(home)/confirm-reserve/info/components/ReserveInfo.tsx
+++ app/[locale]/(website)/(main)/(home)/confirm-reserve/info/components/ReserveInfo.tsx
@@ -82,55 +82,72 @@ export default function ReserveInfo() {
      </div>
     )}
     <div className={`hidden ${showInfo ? '!block' : ''} lg:block`}>
-     {selectedRooms.map((room, i) => (
-      <div key={room.internalID} className='mb-1'>
-       <div className='flex gap-2 items-center mb-3'>
-        <p>
-         <span className='text-neutral-600'>اتاق {i + 1}: </span>
-         <span className='font-medium text-[0.9rem]'>{room.fName}</span>
-        </p>
-        <div className='bg-neutral-300 h-[1px] flex-grow'></div>
-       </div>
-       <div>
-        <div className='px-4 flex justify-between gap-2 mb-3'>
-         <span className='font-medium'>قیمت: </span>
-         <div className='flex items-center'>
-          <span className='me-1 font-light text-[0.7rem] text-red-700 line-through'>
-           <strong>
-            {numberFormatter.format(room.accommodationTypePrice.netRoomRate)}
-           </strong>{' '}
-           ریال
-          </span>
-          <div className='inline-grid place-items-center bg-teal-700 h-[1.5rem] aspect-square rounded-full me-4 text-primary-foreground text-[0.7rem]'>
-           12%
-          </div>
-          <span>
-           <strong>
-            {numberFormatter.format(room.accommodationTypePrice.netRoomRate)}
-           </strong>{' '}
-           ریال
-          </span>
-         </div>
+     {selectedRooms.map((room, i) => {
+      const discountPercentage = room.accommodationTypePrice.roomOnlineShowRate
+       ? Number(
+          (
+           ((room.accommodationTypePrice.netRoomRate -
+            room.accommodationTypePrice.roomOnlineShowRate) *
+            100) /
+           room.accommodationTypePrice.roomOnlineShowRate
+          ).toFixed(0)
+         )
+       : 0;
+      return (
+       <div key={room.internalID} className='mb-1'>
+        <div className='flex gap-2 items-center mb-3'>
+         <p>
+          <span className='text-neutral-600'>اتاق {i + 1}: </span>
+          <span className='font-medium text-[0.9rem]'>{room.fName}</span>
+         </p>
+         <div className='bg-neutral-300 h-[1px] flex-grow'></div>
         </div>
-        <Swiper
-         className='!pb-6 [&]:[--swiper-pagination-bottom:2px]'
-         pagination={{ clickable: true }}
-         modules={[Pagination]}
-         spaceBetween={10}
-         slidesPerView={2}
-         breakpoints={{
-          1024: {
-           slidesPerView: 3,
-          },
-         }}
-        >
-         {Array.from({ length: nights }, (_, i) => i).map((i) => (
-          <SwiperSlide key={i}>
-           <div className='border border-neutral-300 rounded-lg p-2'>
-            <div className='font-medium text-[0.7rem] text-center text-primary-dark border-b border-neutral-300 mb-2 pb-2'>
-             {dateFormatter.format(dateFns.addDays(checkInDate, i))}
-            </div>
-            {/* <div className='flex gap-2 items-center text-[0.7rem] mb-2 justify-center'>
+        <div>
+         <div className='px-4 flex justify-between gap-2 mb-3'>
+          <span className='font-medium'>قیمت: </span>
+          <div className='flex items-center'>
+           {discountPercentage > 0 && (
+            <>
+             <span className='me-1 font-light text-[0.7rem] text-red-700 line-through'>
+              <strong>
+               {numberFormatter.format(
+                room.accommodationTypePrice.roomOnlineShowRate
+               )}
+              </strong>{' '}
+              ریال
+             </span>
+             <div className='inline-grid place-items-center bg-teal-700 h-[1.5rem] aspect-square rounded-full me-4 text-primary-foreground text-[0.7rem]'>
+              {discountPercentage}%
+             </div>
+            </>
+           )}
+           <span>
+            <strong>
+             {numberFormatter.format(room.accommodationTypePrice.netRoomRate)}
+            </strong>{' '}
+            ریال
+           </span>
+          </div>
+         </div>
+         <Swiper
+          className='!pb-6 [&]:[--swiper-pagination-bottom:2px]'
+          pagination={{ clickable: true }}
+          modules={[Pagination]}
+          spaceBetween={10}
+          slidesPerView={2}
+          breakpoints={{
+           1024: {
+            slidesPerView: 3,
+           },
+          }}
+         >
+          {Array.from({ length: nights }, (_, i) => i).map((i) => (
+           <SwiperSlide key={i}>
+            <div className='border border-neutral-300 rounded-lg p-2'>
+             <div className='font-medium text-[0.7rem] text-center text-primary-dark border-b border-neutral-300 mb-2 pb-2'>
+              {dateFormatter.format(dateFns.addDays(checkInDate, i))}
+             </div>
+             {/* <div className='flex gap-2 items-center text-[0.7rem] mb-2 justify-center'>
              <span>
               <span className='line-through text-red-600'>
                {numberFormatter.format(1455566000)}
@@ -140,23 +157,24 @@ export default function ReserveInfo() {
               12%
              </div>
             </div> */}
-            <div className='text-[0.75rem] flex justify-center'>
-             <div>
-              <strong>
-               {numberFormatter.format(
-                room.accommodationTypePrice.dailyPrices[i].price
-               )}
-              </strong>
-              <span className='text-[0.7rem] ps-1'>ریال</span>
+             <div className='text-[0.75rem] flex justify-center'>
+              <div>
+               <strong>
+                {numberFormatter.format(
+                 room.accommodationTypePrice.dailyPrices[i].price
+                )}
+               </strong>
+               <span className='text-[0.7rem] ps-1'>ریال</span>
+              </div>
              </div>
             </div>
-           </div>
-          </SwiperSlide>
-         ))}
-        </Swiper>
+           </SwiperSlide>
+          ))}
+         </Swiper>
+        </div>
        </div>
-      </div>
-     ))}
+      );
+     })}
     </div>
     <div className='lg:hidden'>
      <Button
