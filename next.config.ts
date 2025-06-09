af82c1d483e9d0ee4cba4bbff81928349082b4c3
hamidwise1 <hrawebdev@gmail.com>
2025-06-09 10:58:43 +0330

    dockerize online reserve

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
  /* config options here */
+ // for docker build, we need to enable standalone mode
+ // output: 'standalone',
  eslint: {
   ignoreDuringBuilds: true,
  },
