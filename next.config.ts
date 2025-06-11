fce735b94e6f6b5a2b11f6d35239c1306628cb59
hamidwise1 <hrawebdev@gmail.com>
2025-06-11 14:31:52 +0330

    update online reserve dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
  /* config options here */
  // for docker build, we need to enable standalone mode
- output: 'standalone',
  eslint: {
   ignoreDuringBuilds: true,
  },
