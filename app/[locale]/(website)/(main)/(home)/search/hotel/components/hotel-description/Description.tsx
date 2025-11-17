6f9fba3813a12ba7f80bdd1ecd69c276561bc2e6
hrawebdev <hrawebdev@gmail.com>
2025-11-17 13:25:15 +0330

    update description checkin and checkou

--- app/[locale]/(website)/(main)/(home)/search/hotel/components/hotel-description/Description.tsx
+++ app/[locale]/(website)/(main)/(home)/search/hotel/components/hotel-description/Description.tsx
@@ -1,68 +1,69 @@
-'use client';
-import Button from '@mui/material/Button';
+"use client";
+import Button from "@mui/material/Button";
 import {
- type HotelInfo,
- type Facilities,
-} from '../../../../../../services/HotelApiActions';
-import HotelFacilities from './HotelFacilities';
-import RoomFacilities from './RoomFacilities';
-import { useState } from 'react';
+  type HotelInfo,
+  type Facilities,
+} from "../../../../../../services/HotelApiActions";
+import HotelFacilities from "./HotelFacilities";
+import RoomFacilities from "./RoomFacilities";
+import { useState } from "react";
 
 type Props = {
- facilities: Facilities[];
- roomFacilities: Facilities[];
- hotelInfo: HotelInfo | null;
+  facilities: Facilities[];
+  roomFacilities: Facilities[];
+  hotelInfo: HotelInfo | null;
 };
 
 const descriptionMaxLength = 100;
 
 export default function Description({
- facilities,
- roomFacilities,
- hotelInfo,
+  facilities,
+  roomFacilities,
+  hotelInfo,
 }: Props) {
- const [showMore, setShowMore] = useState(false);
- return (
-  <section className='container grid md:grid-cols-2 gap-4 mb-6'>
-   <article>
-    <div className='grid gap-4  lg:text-base justify-items-center grid-cols-4 mb-4'>
-     <div className='text-center text-primary-dark font-medium'>
-      <p className='mb-2'>ساعت ورود</p>
-      <div>12:23</div>
-     </div>
-     <div className='text-center text-primary-dark font-medium'>
-      <p className='mb-2'>ساعت خروج</p>
-      <div>12:23</div>
-     </div>
-     <div className='text-center text-primary-dark font-medium'>
-      <p className='mb-2'>تعداد اتاق</p>
-      <div>23</div>
-     </div>
-     <div className='text-center text-primary-dark font-medium'>
-      <p className='mb-2'>تعداد طبقه</p>
-      <div>25</div>
-     </div>
-    </div>
-    <p className='leading-7 text-neutral-500 text-justify'>
-     {showMore
-      ? hotelInfo?.description || ''
-      : hotelInfo?.description?.slice(0, descriptionMaxLength) || '' + '...'}
-     {hotelInfo?.description?.length &&
-      hotelInfo?.description?.length > descriptionMaxLength && (
-       <Button
-        color='secondary'
-        className='!font-medium'
-        onClick={() => setShowMore(!showMore)}
-       >
-        {showMore ? 'موارد کمتر' : 'موارد بیشتر'}
-       </Button>
-      )}
-    </p>
-   </article>
-   <article className='grid gap-4 md:grid-cols-2 '>
-    <HotelFacilities facilities={facilities} />
-    <RoomFacilities facilities={roomFacilities} />
-   </article>
-  </section>
- );
+  const [showMore, setShowMore] = useState(false);
+  return (
+    <section className="container grid md:grid-cols-2 gap-4 mb-6">
+      <article>
+        <div className="grid gap-4  lg:text-base justify-items-center grid-cols-4 mb-4">
+          <div className="text-center text-primary-dark font-medium">
+            <p className="mb-2">ساعت ورود</p>
+            <div>{hotelInfo?.checkin}</div>
+          </div>
+          <div className="text-center text-primary-dark font-medium">
+            <p className="mb-2">ساعت خروج</p>
+            <div>{hotelInfo?.checkin}</div>
+          </div>
+          <div className="text-center text-primary-dark font-medium">
+            <p className="mb-2">تعداد اتاق</p>
+            <div>23</div>
+          </div>
+          <div className="text-center text-primary-dark font-medium">
+            <p className="mb-2">تعداد طبقه</p>
+            <div>25</div>
+          </div>
+        </div>
+        <p className="leading-7 text-neutral-500 text-justify">
+          {showMore
+            ? hotelInfo?.description || ""
+            : hotelInfo?.description?.slice(0, descriptionMaxLength) ||
+              "" + "..."}
+          {hotelInfo?.description?.length &&
+            hotelInfo?.description?.length > descriptionMaxLength && (
+              <Button
+                color="secondary"
+                className="!font-medium"
+                onClick={() => setShowMore(!showMore)}
+              >
+                {showMore ? "موارد کمتر" : "موارد بیشتر"}
+              </Button>
+            )}
+        </p>
+      </article>
+      <article className="grid gap-4 md:grid-cols-2 ">
+        <HotelFacilities facilities={facilities} />
+        <RoomFacilities facilities={roomFacilities} />
+      </article>
+    </section>
+  );
 }
--- app/[locale]/(website)/services/HotelApiActions.ts
+++ app/[locale]/(website)/services/HotelApiActions.ts
@@ -1,282 +1,284 @@
-import { axios } from '@/app/services/axios/axiosBaseConfig';
-import { type TRowWithInternalID } from '@/utils/getGridInternalID';
+import { axios } from "@/app/services/axios/axiosBaseConfig";
+import { type TRowWithInternalID } from "@/utils/getGridInternalID";
 
-const getRoomInventoryKey = 'get-room-inventory';
-const getRoomInventoriesApi = '/CRS/OnlineReservation/GetRoomInventories';
+const getRoomInventoryKey = "get-room-inventory";
+const getRoomInventoriesApi = "/CRS/OnlineReservation/GetRoomInventories";
 
-const getRoomDailyPriceKey = 'get-room-daily-price';
-const getRoomDailyPriceApi = '/CRS/OnlineReservation/GetMonthyInventory';
+const getRoomDailyPriceKey = "get-room-daily-price";
+const getRoomDailyPriceApi = "/CRS/OnlineReservation/GetMonthyInventory";
 
-const getRatePlanTypesKey = 'get-rate-plan-types';
-const getRatePlanTypesApi = '/CRS/OnlineReservation/GetRatePlans';
+const getRatePlanTypesKey = "get-rate-plan-types";
+const getRatePlanTypesApi = "/CRS/OnlineReservation/GetRatePlans";
 
-const getHotelImagesKey = 'get-hotel-images';
-const getHotelImagesApi = '/CRS/OnlineReservation/GetHotelImages';
+const getHotelImagesKey = "get-hotel-images";
+const getHotelImagesApi = "/CRS/OnlineReservation/GetHotelImages";
 
-const getHotelFacilitiesKey = 'get-hotel-facilities';
-const getHotelFacilitiesApi = '/CRS/OnlineReservation/GetHotelFacilities';
+const getHotelFacilitiesKey = "get-hotel-facilities";
+const getHotelFacilitiesApi = "/CRS/OnlineReservation/GetHotelFacilities";
 
-const getRoomFacilitiesKey = 'get-room-facilities';
+const getRoomFacilitiesKey = "get-room-facilities";
 const getRoomFacilitiesApi =
- '/CRS/OnlineReservation/GetHotelRoomTypeFacilities';
+  "/CRS/OnlineReservation/GetHotelRoomTypeFacilities";
 
-const hotelInfoKey = 'get-hotel-info';
-const hotelInfoApi = '/CRS/OnlineReservation/GetHotelInfo';
+const hotelInfoKey = "get-hotel-info";
+const hotelInfoApi = "/CRS/OnlineReservation/GetHotelInfo";
 
 type HotelImage = {
- hotelID: number;
- imageURL: string;
+  hotelID: number;
+  imageURL: string;
 };
 
 type HotelInfo = {
- id: number;
- address: string | null;
- cityName: string | null;
- description: string | null;
- fName: string | null;
- faxNo: string | null;
- floors: number | null;
- hotelGradeID: number | null;
- hoteID: number;
- latitude: number | null;
- longitude: number | null;
- logo: string | null;
- roomsCount: number;
- stateName: string | null;
- telNo1: string | null;
- telNo2: string | null;
- telNo3: string | null;
- publicRules: string | null;
+  id: number;
+  address: string | null;
+  cityName: string | null;
+  description: string | null;
+  fName: string | null;
+  faxNo: string | null;
+  floors: number | null;
+  hotelGradeID: number | null;
+  hoteID: number;
+  latitude: number | null;
+  longitude: number | null;
+  logo: string | null;
+  roomsCount: number;
+  stateName: string | null;
+  telNo1: string | null;
+  telNo2: string | null;
+  telNo3: string | null;
+  publicRules: string | null;
+  checkin: string;
+  checkout: string;
 };
 
 type Facilities = {
- key: string;
- value: string;
+  key: string;
+  value: string;
 };
 
 type RatePlanType = {
- fName: string;
- ratePlanID: number;
- ratePlanTypeName: string;
- noBreakfast: boolean;
- nonRefundable: boolean;
- minNights: number;
- freeChargeMinibar: boolean;
- withLunch: boolean;
- withDinner: boolean;
- limitedMenu: boolean;
+  fName: string;
+  ratePlanID: number;
+  ratePlanTypeName: string;
+  noBreakfast: boolean;
+  nonRefundable: boolean;
+  minNights: number;
+  freeChargeMinibar: boolean;
+  withLunch: boolean;
+  withDinner: boolean;
+  limitedMenu: boolean;
 };
 
 type RoomAccomodationType = {
- roomOnlineShowRate: number;
- netRoomRate: number;
- beds: number;
- dailyPrices: {
-  price: number;
-  date: string;
- }[];
- accommodationRatePlanModel: {
-  ratePlanID: number;
-  rateTypeID: number;
-  rateTypePlan: number;
-  rateTypeName: string;
-  boardID: number;
-  ratePlanModel: RatePlanType;
- };
+  roomOnlineShowRate: number;
+  netRoomRate: number;
+  beds: number;
+  dailyPrices: {
+    price: number;
+    date: string;
+  }[];
+  accommodationRatePlanModel: {
+    ratePlanID: number;
+    rateTypeID: number;
+    rateTypePlan: number;
+    rateTypeName: string;
+    boardID: number;
+    ratePlanModel: RatePlanType;
+  };
 } & TRowWithInternalID;
 
 type AccomodationImage = {
- imageURL: string;
+  imageURL: string;
 } & TRowWithInternalID;
 
 type RoomInventory = {
- hoteID: number;
- roomTypeID: number;
- fName: string;
- fixBeds: number;
- roomCount: number;
- accommodationTypePrices: RoomAccomodationType[];
- accommodationTypePrice: RoomAccomodationType;
- accommodationImages: AccomodationImage[];
+  hoteID: number;
+  roomTypeID: number;
+  fName: string;
+  fixBeds: number;
+  roomCount: number;
+  accommodationTypePrices: RoomAccomodationType[];
+  accommodationTypePrice: RoomAccomodationType;
+  accommodationImages: AccomodationImage[];
 } & TRowWithInternalID;
 
 type RoomDailyPrice = {
- date: string;
- roomOnlineShowRate: number;
- netRoomRate: number;
- stopSell: boolean;
- cta: boolean;
- ctd: boolean;
+  date: string;
+  roomOnlineShowRate: number;
+  netRoomRate: number;
+  stopSell: boolean;
+  cta: boolean;
+  ctd: boolean;
 };
 
 function getRoomInventory({
- signal,
- ...queries
+  signal,
+  ...queries
 }: {
- signal: AbortSignal;
- channelID: number;
- providerID: number;
- checkinDate: string;
- checkoutDate: string;
- arzID: number;
- hotelID: number;
- person: string;
- noBreakfast: string;
- fullBoard: string;
- refundable: string;
- ratePlanID: null | string;
+  signal: AbortSignal;
+  channelID: number;
+  providerID: number;
+  checkinDate: string;
+  checkoutDate: string;
+  arzID: number;
+  hotelID: number;
+  person: string;
+  noBreakfast: string;
+  fullBoard: string;
+  refundable: string;
+  ratePlanID: null | string;
 }) {
- const searchParams = new URLSearchParams();
- Object.entries(queries).forEach(([key, val]) => {
-  if (val) searchParams.set(key, String(val));
- });
- return axios.get<RoomInventory[]>(
-  `${getRoomInventoriesApi}?${searchParams.toString()}`,
-  { signal }
- );
+  const searchParams = new URLSearchParams();
+  Object.entries(queries).forEach(([key, val]) => {
+    if (val) searchParams.set(key, String(val));
+  });
+  return axios.get<RoomInventory[]>(
+    `${getRoomInventoriesApi}?${searchParams.toString()}`,
+    { signal },
+  );
 }
 
 function getRoomPriceDaily({
- signal,
- ...queries
+  signal,
+  ...queries
 }: {
- signal: AbortSignal;
- channelID: number;
- hotelID: number;
- providerID: number;
- startDate: string;
- endDate: string;
- roomTypeID: number;
- ratePlanID: number;
- arzID: number;
- beds: number;
+  signal: AbortSignal;
+  channelID: number;
+  hotelID: number;
+  providerID: number;
+  startDate: string;
+  endDate: string;
+  roomTypeID: number;
+  ratePlanID: number;
+  arzID: number;
+  beds: number;
 }) {
- const searchParams = new URLSearchParams();
- Object.entries(queries).forEach(([key, val]) => {
-  searchParams.set(key, String(val));
- });
- return axios.get<RoomDailyPrice[]>(
-  `${getRoomDailyPriceApi}?${searchParams.toString()}`,
-  {
-   signal,
-  }
- );
+  const searchParams = new URLSearchParams();
+  Object.entries(queries).forEach(([key, val]) => {
+    searchParams.set(key, String(val));
+  });
+  return axios.get<RoomDailyPrice[]>(
+    `${getRoomDailyPriceApi}?${searchParams.toString()}`,
+    {
+      signal,
+    },
+  );
 }
 
 const getRatePlanTypes = ({
- signal,
- ...queries
+  signal,
+  ...queries
 }: {
- signal: AbortSignal;
- channelID: number;
- hotelID: number;
- providerID: number;
+  signal: AbortSignal;
+  channelID: number;
+  hotelID: number;
+  providerID: number;
 }) => {
- const searchParams = new URLSearchParams();
- Object.entries(queries).forEach(([key, val]) => {
-  searchParams.set(key, String(val));
- });
- return axios.get<RatePlanType[]>(
-  `${getRatePlanTypesApi}?${searchParams.toString()}`,
-  {
-   signal,
-  }
- );
+  const searchParams = new URLSearchParams();
+  Object.entries(queries).forEach(([key, val]) => {
+    searchParams.set(key, String(val));
+  });
+  return axios.get<RatePlanType[]>(
+    `${getRatePlanTypesApi}?${searchParams.toString()}`,
+    {
+      signal,
+    },
+  );
 };
 
 const getHotelImages = ({
- signal,
- ...queries
-}: {
- signal: AbortSignal;
- channelID: number;
- hotelID: number;
-}) => {
- const searchParams = new URLSearchParams();
- Object.entries(queries).forEach(([key, val]) => {
-  searchParams.set(key, String(val));
- });
- return axios.get(`${getHotelImagesApi}?${searchParams.toString()}`, {
   signal,
- });
+  ...queries
+}: {
+  signal: AbortSignal;
+  channelID: number;
+  hotelID: number;
+}) => {
+  const searchParams = new URLSearchParams();
+  Object.entries(queries).forEach(([key, val]) => {
+    searchParams.set(key, String(val));
+  });
+  return axios.get(`${getHotelImagesApi}?${searchParams.toString()}`, {
+    signal,
+  });
 };
 
 const getHotelFacilities = ({
- signal,
- ...queries
-}: {
- signal: AbortSignal;
- channelID: number;
- hotelID: number;
-}) => {
- const searchParams = new URLSearchParams();
- Object.entries(queries).forEach(([key, val]) => {
-  searchParams.set(key, String(val));
- });
- return axios.get(`${getHotelFacilitiesApi}?${searchParams.toString()}`, {
   signal,
- });
+  ...queries
+}: {
+  signal: AbortSignal;
+  channelID: number;
+  hotelID: number;
+}) => {
+  const searchParams = new URLSearchParams();
+  Object.entries(queries).forEach(([key, val]) => {
+    searchParams.set(key, String(val));
+  });
+  return axios.get(`${getHotelFacilitiesApi}?${searchParams.toString()}`, {
+    signal,
+  });
 };
 
 const getRoomFacilities = ({
- signal,
- ...queries
-}: {
- signal: AbortSignal;
- channelID: number;
- hotelID: number;
-}) => {
- const searchParams = new URLSearchParams();
- Object.entries(queries).forEach(([key, val]) => {
-  searchParams.set(key, String(val));
- });
- return axios.get(`${getRoomFacilitiesApi}?${searchParams.toString()}`, {
   signal,
- });
+  ...queries
+}: {
+  signal: AbortSignal;
+  channelID: number;
+  hotelID: number;
+}) => {
+  const searchParams = new URLSearchParams();
+  Object.entries(queries).forEach(([key, val]) => {
+    searchParams.set(key, String(val));
+  });
+  return axios.get(`${getRoomFacilitiesApi}?${searchParams.toString()}`, {
+    signal,
+  });
 };
 
 const getHotelInfo = ({
- signal,
- ...queries
-}: {
- signal: AbortSignal;
- channelID: number;
- hotelID: number;
-}) => {
- const searchParams = new URLSearchParams();
- Object.entries(queries).forEach(([key, val]) => {
-  searchParams.set(key, String(val));
- });
- return axios.get(`${hotelInfoApi}?${searchParams.toString()}`, {
   signal,
- });
+  ...queries
+}: {
+  signal: AbortSignal;
+  channelID: number;
+  hotelID: number;
+}) => {
+  const searchParams = new URLSearchParams();
+  Object.entries(queries).forEach(([key, val]) => {
+    searchParams.set(key, String(val));
+  });
+  return axios.get(`${hotelInfoApi}?${searchParams.toString()}`, {
+    signal,
+  });
 };
 
 export {
- type RatePlanType,
- type RoomInventory,
- type RoomDailyPrice,
- type RoomAccomodationType,
- type AccomodationImage,
- type HotelImage,
- type Facilities,
- type HotelInfo,
- getRoomInventoryKey,
- getRoomInventory,
- getRoomDailyPriceKey,
- getRoomPriceDaily,
- getRatePlanTypesKey,
- getRatePlanTypes,
- getHotelImagesApi,
- getHotelImagesKey,
- getHotelImages,
- getHotelFacilitiesApi,
- getHotelFacilitiesKey,
- getHotelFacilities,
- getRoomFacilitiesKey,
- getRoomFacilitiesApi,
- getRoomFacilities,
- hotelInfoApi,
- hotelInfoKey,
- getHotelInfo,
+  type RatePlanType,
+  type RoomInventory,
+  type RoomDailyPrice,
+  type RoomAccomodationType,
+  type AccomodationImage,
+  type HotelImage,
+  type Facilities,
+  type HotelInfo,
+  getRoomInventoryKey,
+  getRoomInventory,
+  getRoomDailyPriceKey,
+  getRoomPriceDaily,
+  getRatePlanTypesKey,
+  getRatePlanTypes,
+  getHotelImagesApi,
+  getHotelImagesKey,
+  getHotelImages,
+  getHotelFacilitiesApi,
+  getHotelFacilitiesKey,
+  getHotelFacilities,
+  getRoomFacilitiesKey,
+  getRoomFacilitiesApi,
+  getRoomFacilities,
+  hotelInfoApi,
+  hotelInfoKey,
+  getHotelInfo,
 };
